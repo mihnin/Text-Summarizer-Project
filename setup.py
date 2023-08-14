38b62a2a713cd97ffcb10077b5dea8a59ec91ed5
mihnin <mihnin@hotmail.com>
2023-08-14 03:46:15 +0300

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+# Открываем файл README и считываем его содержимое в переменную
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Устанавливаем номер версии
+__version__ = "0.0.0"
+
+# Устанавливаем переменные для имени репозитория, имени пользователя автора, 
+# названия исходного репозитория и электронной почты автора
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "mihnin"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "mihnin@gmail.com"
+
+# Устанавливаем информацию о пакете для setuptools
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
