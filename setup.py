60b4019bd803ce16c1a5a779ef6b30a6e0dbe0e0
mihnin <mihnin@hotmail.com>
2023-08-20 00:23:25 +0300

    ooo

--- setup.py
+++ setup.py
@@ -1,39 +1,15 @@
-import setuptools # импортируем пакет setuptools
+import setuptools
 
-<<<<<<< HEAD
-with open("README.md", "r", encoding="utf-8") as f: # открываем файл README.md
-     long_description = f.read() # читаем файл README.md
-
-
-__version__ = "0.0.0" # создаем константу __version__ со значением "0.0.0"
-
-REPO_NAME = "project_1" # создаем константу REPO_NAME со значением "project_1" 
-AUTHOR_USER_NAME = "mihnin" # создаем константу AUTHOR_USER_NAME со значением "mihnin"
-SRC_REPO = "textSummarizer" # создаем константу SRC_REPO со значением "textSummarizer"
-AUTHOR_EMAIL = "mihnin@gmail.com" # создаем константу AUTHOR_EMAIL со значением "
-
-
-
-setuptools.setup( # вызываем функцию setup() из пакета setuptools
-    name=SRC_REPO, # указываем имя пакета
-    version=__version__, # указываем версию пакета
-    author=AUTHOR_USER_NAME, # указываем автора пакета
-    author_email=AUTHOR_EMAIL, # указываем автора пакета
-    description="A small python package for NLP app", # описание пакета
-    long_description=long_description, # длинное описание пакета
-    long_description_content="text/markdown", # тип контента
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}", # ссылка на репозиторий
-=======
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "mihnin"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "mihnin@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 
@@ -46,11 +22,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
->>>>>>> d021832c63440171a1a0531580f3a4685fbe0f71
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues", 
-        # ссылка на багтрекер
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "src"}, # указываем, что все модули находятся в папке src
-    packages=setuptools.find_packages(where="src") # указываем, что все модули находятся в папке src
-) # указываем параметры функции setup()
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
