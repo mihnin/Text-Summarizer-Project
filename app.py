6c2b0847a67a039fc24da7282cf82a170f01411c
mihnin <mihnin@hotmail.com>
2023-08-20 17:28:23 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI # импортируем библиотеку FastAPI для создания API
+import uvicorn # импортируем библиотеку uvicorn для запуска сервера
+import sys # импортируем библиотеку sys для работы с системными параметрами
+import os  # импортируем библиотеку os для работы с файловой системой
+from fastapi.templating import Jinja2Templates # импортируем библиотеку Jinja2Templates для работы с шаблонами
+from starlette.responses import RedirectResponse # импортируем библиотеку RedirectResponse для редиректа
+from fastapi.responses import Response # импортируем библиотеку Response для работы с ответами
+from textSummarizer.pipeline.prediction import PredictionPipeline 
+# импортируем класс PredictionPipeline для предсказания
+
+
+text:str = "What is Text Summarization?" # текст для предсказания
+
+app = FastAPI() # создаем экземпляр класса FastAPI
+
+@app.get("/", tags=["authentication"]) # создаем корневой маршрут
+async def index(): # функция для корневого маршрута
+    return RedirectResponse(url="/docs") # редирект на документацию
+
+
+
+@app.get("/train") # создаем маршрут для обучения
+async def training(): # функция для обучения
+    try: # обрабатываем исключения
+        os.system("python main.py") # запускаем обучение
+        return Response("Training successful !!") # возвращаем ответ
+
+    except Exception as e: # обрабатываем исключения
+        return Response(f"Error Occurred! {e}") # возвращаем ответ
+    
+
+
+
+@app.post("/predict") # создаем маршрут для предсказания
+async def predict_route(text): # функция для предсказания
+    try: # обрабатываем исключения
+
+        obj = PredictionPipeline() # создаем экземпляр класса PredictionPipeline
+        text = obj.predict(text) # получаем суммарный текст
+        return text # возвращаем суммарный текст
+    except Exception as e: # обрабатываем исключения
+        raise e # возвращаем исключение
+    
+
+if __name__=="__main__": # если файл запускается как главный
+    uvicorn.run(app, host="127.0.0.1", port=8080) # запускаем сервер
--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -1,25 +1,25 @@
-from textSummarizer.config.configuration import ConfigurationManager
-from transformers import AutoTokenizer
-from transformers import pipeline
+from textSummarizer.config.configuration import ConfigurationManager # импортируем класс конфигурации
+from transformers import AutoTokenizer # импортируем токенайзер
+from transformers import pipeline # импортируем пайплайн
 
 
-class PredictionPipeline:
-    def __init__(self):
-        self.config = ConfigurationManager().get_model_evaluation_config()
+class PredictionPipeline: # класс для предсказания
+    def __init__(self): # конструктор класса
+        self.config = ConfigurationManager().get_model_evaluation_config() # получаем конфигурацию
 
 
     
-    def predict(self,text):
-        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+    def predict(self,text): # функция предсказания
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path) # получаем токенайзер
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128} # параметры для генерации
 
-        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer) # получаем пайплайн
 
-        print("Dialogue:")
-        print(text)
+        print("Dialogue:") # выводим диалог
+        print(text) # выводим текст
 
-        output = pipe(text, **gen_kwargs)[0]["summary_text"]
-        print("\nModel Summary:")
-        print(output)
+        output = pipe(text, **gen_kwargs)[0]["summary_text"] # получаем суммарный текст
+        print("\nModel Summary:") # выводим суммарный текст
+        print(output) # выводим суммарный текст
 
-        return output
+        return output # возвращаем суммарный текст
