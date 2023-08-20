0e0cb46befd556ef94d15f5dd8eafc977b80daae
mihnin <mihnin@hotmail.com>
2023-08-21 02:02:29 +0300

    template_01

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "template_01"
 AUTHOR_USER_NAME = "mihnin"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "mihnin@gmail.com"
